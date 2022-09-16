0ee490eb6f90d05e09ae50b82ee50fc9d93d36a8
Noon Pokaratsiri <noonsky.noon@gmail.com>
2022-09-16 02:17:55 +0200

    srun tfidf script

--- srun/baseline_experiments_tfidf.sh
+++ srun/baseline_experiments_tfidf.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # SLURM environment arguments
+export SCISPACY_CACHE=/netscratch/pokarats/cache/scispacy
 MYDATA=/netscratch/pokarats/ds
 IMAGE=/netscratch/pokarats/nvcr.io_nvidia_pytorch_22.02-py3_base1.sqsh
-NUM_CPUS=32
-MEM_PER_CPU=8GB
+NUM_CPUS=8
+MEM_PER_CPU=16GB
 
 # variables for srun and python
 for vers in 50 full
@@ -14,14 +15,15 @@ do
   cache=/netscratch/pokarats/cache/scispacy
   pickle_file="$vers"_cuis_to_discard
 
-  srun -K -p batch \
+  srun -K -p RTX3090-MLT \
     --container-mounts=/netscratch/pokarats:/netscratch/pokarats,/ds:/ds:ro,"$(pwd)":"$(pwd)"\
     --container-workdir="$(pwd)" \
     --container-image=$IMAGE \
-    --export=ALL,SCISPACY_CACHE=/netscratch/pokarats/cache/scispacy \
     --cpus-per-task=$NUM_CPUS \
     --mem-per-cpu=$MEM_PER_CPU \
     --nodes=1 \
+    --mail-type=END \
+    --mail-user=noon.pokaratsiri@dfki.de \
   python src/baseline_exp.py \
     --data_dir $MYDATA \
     --mimic3_dir $mimic3_dir \
@@ -29,9 +31,11 @@ do
     --split $split \
     --model tfidf \
     --stacked True \
+    --extra False \
     --cache_dir $cache \
     --misc_pickle_file "$pickle_file".pickle \
     --add_name tfidf \
     --filename "$vers"_cuis_to_discard_None \
     --n_process $NUM_CPUS
+  sleep 1
 done
