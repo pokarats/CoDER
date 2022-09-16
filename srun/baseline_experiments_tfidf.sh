6b9a0e4f7bd9874fab148a5be0f17ac5b456869d
Noon Pokaratsiri <pokaratsiri@kiew.kl.dfki.de>
2022-09-16 21:16:00 +0200

    srun extra true skiplogref false

--- srun/baseline_experiments_tfidf.sh
+++ srun/baseline_experiments_tfidf.sh
@@ -19,7 +19,7 @@ do
     --container-mounts=/netscratch/pokarats:/netscratch/pokarats,/ds:/ds:ro,"$(pwd)":"$(pwd)" \
     --container-workdir="$(pwd)" \
     --container-image=$IMAGE \
-    --job-name=tfidf_"$vers"_extra_false \
+    --job-name=tfidf_"$vers"_extra \
     --cpus-per-task=$NUM_CPUS \
     --mem-per-cpu=$MEM_PER_CPU \
     --nodes=1 \
@@ -31,6 +31,7 @@ do
     --version $vers \
     --split $split \
     --model tfidf \
+    --extra True \
     --skip_logreg False \
     --stacked True \
     --cache_dir $cache \
