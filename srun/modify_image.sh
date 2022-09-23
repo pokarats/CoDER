a263945bf071d4d0843f9bc6ef7516b5718f8f9d
Noon Pokaratsiri <noonsky.noon@gmail.com>
2022-09-24 00:49:51 +0200

    specified partition to modify image srun

--- srun/modify_image.sh
+++ srun/modify_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-srun \
+srun -K -p RTXA6000-MLT \
   --container-image=/netscratch/pokarats/nvcr.io_nvidia_pytorch_22.02-py3_"$1".sqsh \
   --container-save=/netscratch/pokarats/nvcr.io_nvidia_pytorch_22.02-py3_"$2".sqsh \
   --pty /bin/bash
