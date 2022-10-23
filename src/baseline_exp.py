c0b055b2e11b9f99785bd4549b33322a9fc0864d
Noon Pokaratsiri <noonsky.noon@gmail.com>
2022-10-24 01:34:30 +0200

    cleaned up comments

--- src/baseline_exp.py
+++ src/baseline_exp.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-DESCRIPTION: Python template with an argument parser and logger. Put all the "main" logic into the method called "main".
-             Only use the true "__main__" section to add script arguments. The logger writes to a hidden folder './log/'
+DESCRIPTION: Wrapper code for running experiments on the rule-based and non-DL baseline models. The logger writes to a
+             hidden folder './log/'
              and uses the name of this file, followed by the date (by default). The argument parser comes with a default
-             option --quiet to keep the stdout clean.
+             option --quiet to keep the stdout clean. (if run from main with logger config, otherwise cout.txt in
+             Sacred logging)
 
-
-@copyright: Copyright 2018 Deutsches Forschungszentrum fuer Kuenstliche
-            Intelligenz GmbH or its licensors, as applicable.
 
 @author: Noon Pokaratsiri Goldstein
 """
