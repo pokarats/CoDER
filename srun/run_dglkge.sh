1f3427c0f6740ffe8c9427ecd4bf2d7b2479220f
Noon Pokaratsiri <noonsky.noon@gmail.com>
2022-12-05 03:38:22 +0100

    srun dgl kge script

---
+++ srun/run_dglkge.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# SLURM environment arguments
+export DGLBACKEND=pytorch
+MYDATA=/netscratch/pokarats/ds
+IMAGE=/netscratch/pokarats/nvcr.io_nvidia_pytorch_22.02-py3_dglkge.sqsh
+NUM_CPUS=8
+MEM=32GB
+
+# variables for srun and python
+
+vers="snomed_ct_base"
+echo "Submitting version: $vers kge"
+
+srun -K -p RTXA6000-MLT \
+  --container-mounts=/netscratch/pokarats:/netscratch/pokarats,/ds:/ds:ro,"$(pwd)":"$(pwd)" \
+  --container-workdir="$(pwd)" \
+  --container-image=$IMAGE \
+  --job-name=dglke_"$vers" \
+  --cpus-per-task=$NUM_CPUS \
+  --gpus=1 \
+  --mem=$MEM \
+  --nodes=1 \
+  --mail-type=END,FAIL \
+  --mail-user=noon.pokaratsiri@dfki.de \
+dglke_train --model_name TransE_l2 --batch_size 1024 --log_interval 100 --neg_sample_size 60 \
+        --regularization_coef 1e-09 --hidden_dim 100 --gamma 8 --lr 5e-4 --batch_size_eval 16 \
+        --test -adv -a 1.0 --gpu $CUDA_VISIBLE_DEVICES --max_step 2000 --dataset 'raw_udd_umls' --format 'raw_udd_hrt' \
+        --data_path "$MYDATA"/umls --delimiter '\t' --save_path scratch/umls_kge \
+        --data_files train.tsv valid.tsv test.tsv
