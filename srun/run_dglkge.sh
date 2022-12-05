b3ab2f5a1081c7b2b86e045a4d1e6894d4ca71eb
Noon Pokaratsiri <noonsky.noon@gmail.com>
2022-12-05 17:27:41 +0100

    dglkge srun fixed cl args param

--- srun/run_dglkge.sh
+++ srun/run_dglkge.sh
@@ -26,6 +26,6 @@ srun -K -p RTX3090-MLT \
   --mail-user=noon.pokaratsiri@dfki.de \
 dglke_train --model_name TransE_l2 --batch_size 1024 --log_interval 100 --neg_sample_size 60 \
         --regularization_coef 1e-09 --hidden_dim 100 --gamma 8 --lr 5e-4 --batch_size_eval 16 \
-        --test -adv -a 1.0 --gpu $CUDA_VISIBLE_DEVICES --mix_cpu_gpu --valid --max_step 2000 --dataset 'raw_udd_umls' \
-        --format 'raw_udd_hrt' --data_path "$MYDATA"/umls --delimiter '\t' --save_path scratch/umls_kge \
+        --test -adv -a 1.0 --gpu $CUDA_VISIBLE_DEVICES --mix_cpu_gpu --valid --max_step 2000 --dataset raw_udd_umls \
+        --format raw_udd_hrt --data_path "$MYDATA"/umls --save_path scratch/umls_kge \
         --data_files train.tsv dev.tsv test.tsv
