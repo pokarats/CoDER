7e59405337cf569e5a7704ccc2faf424d9c52b76
Noon Pokaratsiri <noonsky.noon@gmail.com>
2022-12-05 14:00:34 +0100

    srun sh to test dglkge

--- srun/run_test_dglkge.sh
+++ srun/run_test_dglkge.sh
@@ -2,6 +2,7 @@
 
 # SLURM environment arguments
 export DGLBACKEND=pytorch
+export CUDA_VISIBLE_DEVICES=0
 MYDATA=/netscratch/pokarats/ds
 OUT_DIR=/netscratch/pokarats/scratch/dglke_test
 IMAGE=/netscratch/pokarats/nvcr.io_nvidia_pytorch_22.03-py3_dglkge.sqsh
@@ -26,6 +27,6 @@ srun -K -p RTXA6000-MLT \
   --mail-user=noon.pokaratsiri@dfki.de \
 dglke_train --model_name TransE_l2 --batch_size 2 --log_interval 10 --neg_sample_size 2 \
         --regularization_coef 1e-06 --hidden_dim 100 --gamma 20.0 --lr 0.14 --batch_size_eval 1 \
-        --test -adv -a 1.0 --gpu $CUDA_VISIBLE_DEVICES --max_step 100 --dataset 'raw_udd_test' --format 'raw_udd_hrt' \
-        --data_path "$MYDATA"/raw_udd_2 --delimiter ';' --save_path $OUT_DIR \
+        --test -adv -a 1.0 --gpu $CUDA_VISIBLE_DEVICES --mix_cpu_gpu --max_step 100 --dataset 'raw_udd_test' \
+        --format 'raw_udd_hrt' --data_path "$MYDATA"/raw_udd_2 --delimiter ';' --save_path $OUT_DIR \
         --data_files train.tsv valid.tsv test.tsv
