d9e4d7297cd19f62c197feb6c7dbb0fb637aa716
Noon Pokaratsiri <noonsky.noon@gmail.com>
2022-12-25 15:40:47 +0100

    srun sacred sh umls embedding with kge prune files

--- srun/run_sacred_laat_exp_pm.sh
+++ srun/run_sacred_laat_exp_pm.sh
@@ -7,13 +7,13 @@ NUM_CPUS=16
 MEM="$2"GB
 
 # variables for srun and python
-for ver in full 50
+for ver in 50 full
 do
   for embedding in snomedbase snomednoex snomedcase4
   do
     desc="$ver cui $embedding"
     echo "Submitting: $desc experiment"
-    if [ $ver == 50 ]; then
+    if [[ $ver == 50 ]]; then
       echo "$ver version u and da is default 256"
       srun -K -p $1 \
         --container-mounts=/netscratch/pokarats:/netscratch/pokarats,/ds:/ds:ro,"$(pwd)":"$(pwd)" \
@@ -27,8 +27,8 @@ do
         --mail-type=END,FAIL \
         --mail-user=noon.pokaratsiri@dfki.de \
       srun/install_pretask.sh python src/laat_exp.py with data_dir="$MYDATA" input_type=umls version="$ver" \
-      embedding_type="$embedding" dr_params.prune_cui=True \
-      dr_params.cui_prune_file="$ver"_cuis_to_discard_"$embedding".pickle      
+      embedding_type=umls dr_params.prune_cui=True \
+      dr_params.cui_prune_file="$ver"_cuis_to_discard_"$embedding".pickle
     else
       echo "$ver needs to be 512"
       srun -K -p $1 \
@@ -43,7 +43,7 @@ do
         --mail-type=END,FAIL \
         --mail-user=noon.pokaratsiri@dfki.de \
       srun/install_pretask.sh python src/laat_exp.py with data_dir="$MYDATA" input_type=umls version="$ver" \
-      embedding_type="$embedding" dr_params.prune_cui=True \
+      embedding_type=umls dr_params.prune_cui=True \
       dr_params.cui_prune_file="$ver"_cuis_to_discard_"$embedding".pickle laat_params.u=512 laat_params.da=512
     fi
     sleep 3
