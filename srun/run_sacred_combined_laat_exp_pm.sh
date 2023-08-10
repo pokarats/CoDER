fc63f249055069cceba27dee23bc1c09303f2829
Noon Pokaratsiri <noonsky.noon@gmail.com>
2023-08-10 14:01:32 +0200

    updated srun bash script for old exp to include new image with dgl

--- srun/run_sacred_combined_laat_exp_pm.sh
+++ srun/run_sacred_combined_laat_exp_pm.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # SLURM environment arguments
+export DGLBACKEND=pytorch
 MYDATA=/netscratch/pokarats/ds
-IMAGE=/netscratch/pokarats/nvcr.io_nvidia_pytorch_22.02-py3_neptune2.sqsh
+IMAGE=/netscratch/pokarats/nvcr.io_nvidia_pytorch_22.03-py3_dglkgesource.sqsh
 NUM_CPUS=16
 MEM="$2"GB
 
